eaa613f70abf5c4c281c0a0d77a4c3eaec5ee141
CvanGhost <60934228+CvanGhost@users.noreply.github.com>
2022-01-17 00:57:27 +0100

    instant change of language selection text removed glitch with moving flags

--- js/language.js
+++ js/language.js
@@ -1,19 +1,11 @@
 const names = ['Select language','Vyber jazyk','Elige idioma']
 
+
 function greetPerson () {
   let name = names.shift()
   names.push(name)
-  const h1 = document.querySelector('h1')
-  const oldSpan = document.querySelector('span')
-  
-  if (oldSpan) {
-    h1.removeChild(oldSpan)
-  }
-  
-  const span = document.createElement('span')
-  span.classList.add('fade')
-  span.textContent = name
-  h1.appendChild(span)
+  const select = document.querySelector('.header');
+  select.textContent = name;
 }
 
-setInterval(greetPerson, 4000)
+setInterval(greetPerson, 3000)
